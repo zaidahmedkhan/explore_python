4dd89390fe361ab0f158898da23b364fa613b7c0
Zaid Ahmed Khan <126311558+zaidahmedkhan@users.noreply.github.com>
2026-07-27 12:52:39 +0500

    weather activityb suggestion

---
+++ conditional_problems/weather_activity_suggestion.py
@@ -0,0 +1,12 @@
+# suggest an activity based on the weather: eg(sunny: go for a walk, rainy : read a book, snowy: buil;d a snowman)
+
+weather = "Sunny"
+
+if(weather == "sunny"):
+    activity = "go for a walk"
+elif(weather == "rainy"):
+    activity = "read a book"
+elif(weather == "snowy"):
+    activity = "build a snowman"
+
+print(activity)    
